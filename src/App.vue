6d985aac0587e89d226975494d8668d7e01b6c56
Truls <trugunn_css@hotmail.com>
2019-10-07 09:26:17 +0200

    Lagt til navbar.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <Navbar></Navbar>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Nav/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,6 +24,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
