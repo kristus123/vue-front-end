7ca5d6943748028d7f9a1a31b6a1af2e9b87a050
haakon <haakon_underdal@hotmail.com>
2019-10-09 09:40:00 +0200

    minor bug fix

--- src/components/cards/player/PlayerCard.vue
+++ src/components/cards/player/PlayerCard.vue
@@ -10,23 +10,57 @@
                     <img src="https://img.etimg.com/thumb/msid-67641060,width-1200,height-900,resizemode-4,imgsize-18028/cristiano-ronaldo.jpg" alt="avatar" class="player-image">
                 </div>
             </b-row>
-            <b-row>
+            <b-row class="row-below-img">
                 <b-col>{{teamName}}</b-col>
                 <b-col>{{playerPosition}}</b-col>
                 <b-col>NO. {{playerNumber}}</b-col>
             </b-row>
             <hr>
             <b-row>
-                <b-col>Season Goals</b-col>
-                <b-col>Total Goals</b-col>
-            </b-row>
-            <b-row>
-                <b-col>{{playerSeasonGoals}}</b-col>
-                <b-col>{{playerTotalGoals}}</b-col>
+                <b-col> 
+                    <b-row>
+                        <b-col><i class="far fa-futbol fa-2x vertical-center"></i></b-col>
+                        <b-col>
+                            <b-row>
+                                <b-col class="text-right">
+                                    Season
+                                </b-col>
+                            </b-row>
+                            <b-row >
+                                <b-col class="text-right">
+                                    {{playerSeasonGoals}}
+                                </b-col>
+                            </b-row>
+                        </b-col>
+                    </b-row>
+                </b-col>
+                <b-col> 
+                    <b-row>
+                        <b-col><i class="far fa-futbol fa-2x vertical-center"></i></b-col>
+                        <b-col>
+                            <b-row>
+                                <b-col class="text-right">
+                                    Lifetime
+                                </b-col>
+                            </b-row>
+                            <b-row >
+                                <b-col class="text-right">
+                                    {{playerTotalGoals}}
+                                </b-col>
+                            </b-row>
+                        </b-col>
+                    </b-row>
+                </b-col>
             </b-row>
             <b-row class= "row-margin">
-                <b-col><player-stats-bar :width="100" :height="100" :chartdata="chartdata" :options="barOptions" /></b-col>
-                <b-col><player-stats-pie :width="100" :height="100" :chartdata="pieDataCollection" :options="pieOptions"/></b-col>
+                <b-col>
+                    <player-stats-bar :width="100" :height="100" :chartdata="chartdata" :options="barOptions" />
+                    <font color="#f87979">Goal per match</font>
+                </b-col>
+                <b-col>
+                    <player-stats-pie :width="100" :height="100" :chartdata="pieDataCollection" :options="pieOptions"/>
+                    <font color="33ff4">Goal types</font>
+                </b-col>
             </b-row>
         </b-card>
     </b-container>
@@ -56,41 +90,42 @@ export default {
             labels: ['Ronaldo', 'Other'],
             datasets: [
                 {
-                label: 'Goals',
-                backgroundColor: '#f87979',
-                pointBackgroundColor: 'white',
-                borderWidth: 1,
-                pointBorderColor: '#249EBF',
-                //Data to be represented on y-axis
-                data: [10,20]
+                    label: 'Goals',
+                    backgroundColor: ['#f87979' , '#a87979'],
+                    pointBackgroundColor: 'white',
+                    borderWidth: 1,
+                    pointBorderColor: '#249EBF',
+                    //Data to be represented on y-axis
+                    data: [1.3,0.8]
                 }
             ]
             },
             //Chart.js options that controls the appearance of the chart
             barOptions: {
-            scales: {
-                yAxes: [{
-                ticks: {
-                    beginAtZero: true
+                scales: {
+                    yAxes: [{
+                        display: false,
+                        ticks: {
+                            beginAtZero: true
+                        },
+                        gridLines: {
+                            display: false
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            display: false
+                        },
+                        gridLines: {
+                            display: false
+                        }
+                    }]
                 },
-                gridLines: {
+                legend: {
                     display: false
-                }
-                }],
-                xAxes: [ {
-                    ticks: {
-                        display: false
-                    },
-                    gridLines: {
-                        display: false
-                }
-                }]
-            },
-            legend: {
-                display: false
-            },
-            responsive: false,
-            maintainAspectRatio: false
+                },
+                responsive: false,
+                maintainAspectRatio: false
             },
             // --------------- PIE DATA AND OPTIONS ---------------
             pieDataCollection: {
@@ -98,15 +133,14 @@ export default {
                 labels: ['Penalty', 'Header', 'Free-kick'],
                 datasets: [
                     {
-                    label: 'Goal types',
-                    backgroundColor: ['#33ff41', '#39af41', '#b9af41'] ,
-                    pointBackgroundColor: 'white',
-                    borderWidth: 1,
-                    pointBorderColor: '#249EBF',
-                    //Data to be represented on y-axis
-                    data: [10, 20, 30]
+                        label: 'Goal types',
+                        backgroundColor: ['#33ff41', '#39af41', '#b9af41'] ,
+                        pointBackgroundColor: 'white',
+                        borderWidth: 1,
+                        pointBorderColor: '#249EBF',
+                        //Data to be represented on y-axis
+                        data: [10, 20, 30]
                     },
-                    
                 ],
                 
             },
@@ -118,9 +152,7 @@ export default {
                 maintainAspectRatio: false
             }
         }
-    }
-
-    
+    }    
 }
 </script>
 
@@ -137,6 +169,7 @@ export default {
         margin: 0 auto;
         height: 100%;
         width: auto;
+        
     }
     .center {
         margin: auto;
@@ -145,6 +178,19 @@ export default {
     }
 
     .row-margin {
-        margin-top:30px;
+        margin-top:50px;
+    }
+    .row-below-img {
+        margin-top: 15px;
+    }
+    .vertical-center {
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        -ms-transform: translateY(-50%);
+        transform: translateY(-50%);
+    }
+    .align-text-right {
+        text-align: right;
     }
 </style>
