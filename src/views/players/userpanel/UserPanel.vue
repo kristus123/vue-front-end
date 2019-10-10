53c053ee475d7571c91a9ec7009ff36132270a0b
krispetter@gmail.com <krispetter@gmail.com>
2019-10-10 15:43:40 +0200

    changes

---
+++ src/views/players/userpanel/UserPanel.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <center>
+      <div class="topnav">
+        <router-link to="/UserPanel/AddTeam">Add team</router-link>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+    </center>
+
+    <router-view  style="margin-top:10px;" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* Add a black background color to the top navigation */
+.topnav {
+  margin-top: 0px;
+  max-width: 100%;
+  background-color: #17a2b8;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 50px;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4caf50;
+  color: white;
+}
+</style>
