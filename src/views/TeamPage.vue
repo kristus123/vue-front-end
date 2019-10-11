5f54f83d57b5ee6460d28b45180e2de4d818b80d
Truls <trugunn_css@hotmail.com>
2019-10-11 14:53:45 +0200

    trying to add some spicy stuff - background etc.

--- src/views/TeamPage.vue
+++ src/views/TeamPage.vue
@@ -1,25 +1,73 @@
 <template>
-    <b-container>
-        This is a page for a singel team.
-        id: {{id}}
-        <img :src="allTeams[id-1].img">
+    
+    <b-container id="teamContainer">
+        <b-row class="justify-content-center">
+            <h1><img id="logo" :src=allTeams[id].img>{{allTeams[id].name}}</h1>            
+        </b-row>
+        <b-card no-body>
+            <b-tabs card justified>
+                <b-tab title="Information" active>
+                    <b-row>
+                        <p><strong>Coach: </strong> {{allTeams[id].coach}}</p>
+                    </b-row>
+                    <b-row>
+                        <p><strong>Stadium: </strong> {{allTeams[id].stadium}}</p>
+                    </b-row>
+                    <b-row>
+                        <p><strong>Year formed: </strong> {{allTeams[id].formed}}</p>
+                    </b-row>
+                    <b-row>
+                        <p><strong>Description: </strong> {{allTeams[id].description}}</p>
+                    </b-row>
+                </b-tab>
+                <b-tab class="test" title="Players">
+                    <b-row>
+                        <b-col cols="3" v-for="count in counter" :key="count.id">
+                            <player-card/>
+                        </b-col>
+                    </b-row>
+
+                </b-tab>
+                <b-tab title="Stats">
+                    <p>Stats here!</p>
+                </b-tab>
+                <b-tab title="Recent Matches">
+                    <p>Recent matches here!</p>
+                </b-tab>
+            </b-tabs>
+        </b-card>
+        
     </b-container>
 </template>
 
 <script>
+import PlayerCard from '@/components/cards/player/PlayerCard';
+
+
 export default {
     name: "teamPage",
     props: ['teamId', 'teams'],
+    components: {
+        PlayerCard
+    },
+
     data() {
         return {
             id : "some id",
-            allTeams : []
+            allTeams : [],
+            counter: [
+                {"id" : 1},
+                {"id" : 2},
+                {"id" : 3},
+                {"id" : 4},
+                {"id" : 5}
+            ]
         }
     },
 
     mounted() {
         if(this.teamId) {
-            this.id = this.teamId;
+            this.id = this.teamId-1;
         }
         if(this.teams) {
             this.allTeams = this.teams;
@@ -30,5 +78,25 @@ export default {
 </script>
 
 <style>
+.nav-tabs .nav-link, .nav-tabs .nav-link {
+    color:rgb(133, 133, 133);
+    border: solid 1px #e9ecef;
+}
+
+.nav-link:hover {
+    color: rgb(65, 65, 65);
+}
+
+#logo{
+    width: 20%;
+    align-content: left;
+}
+
+#teamContainer {
+ background: url("../assets/ball-field-grass-46792.jpg");
+ background-size: cover;
+ background-repeat: no-repeat;
+    
+}
 
 </style>
