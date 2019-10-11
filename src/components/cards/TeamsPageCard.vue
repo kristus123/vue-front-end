de008df398e8413371e7268ff70439b3aa411ba8
Truls <trugunn_css@hotmail.com>
2019-10-11 11:06:14 +0200

    added a page that displays all of the teams.

--- src/components/cards/TeamsPageCard.vue
+++ src/components/cards/TeamsPageCard.vue
@@ -1,12 +1,22 @@
 <template>
     <b-container id="container">
+        <div v-if="showTeam">
+            You cliked!
+            {{this.someId}}
+        </div>
         <b-row>
-            <b-col cols="3" v-for="team in teams" :key="team.id">
-                <b-card id="cardTeam">
-                    <h3 id="teamTitle">{{team.name}}</h3>
-                    <hr>
-                    <p>{{infoTeams[0].teams[0].strTeamBadge}}</p>
-                </b-card>
+            <b-col id="cards" cols="3" v-for="team in teams" :key="team.id">
+                <div id="background-card" v-on:click="onCardClick(team.id)">
+                    <h5 id="teamTitle" class="text-white"><img id="teamLogo" :src="team.img"/>{{team.name}}</h5>
+                    <b-card id="cardTeam">
+                        <div id="info">
+                            <p><strong>Stadium: </strong>{{team.stadium}}</p>
+                            <p><strong>Year formed: </strong>{{team.formed}}</p>
+                            <p><strong>Coach: </strong>{{team.coach}}</p>
+                            <p id="description"><strong>Description: </strong>{{team.description}}</p>
+                        </div>
+                    </b-card>
+                </div>
             </b-col>
         </b-row>
     </b-container>
@@ -17,7 +27,21 @@
 
 export default {
     name : "teamCard",
-    props : ["teams", "infoTeams"]
+    props : ["teams"],
+    data() {
+        return {
+            showTeam: false,
+            someId : ''
+        }
+    },
+
+    methods: {
+        onCardClick(id) {
+            this.showTeam = true;
+            this.someId = id;
+
+        }
+    }
     
 }
 </script>
@@ -34,5 +58,38 @@ export default {
 #teamTitle {
     text-align: center;
 }
+
+#teamLogo{
+    max-width: 20%;
+    margin-right: 5px;
+}
+
+#background-card {
+    background-color: #3d1d6e;
+    border-radius: 10px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    margin-bottom: 10px;
+}
+
+#description {
+    line-height: 1.5em;
+    height: 3em;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    width: 100%;
+}
+
+#info {
+    text-align: left;
+}
+
+#cards:hover{
+    transform: rotateZ(4deg);
+    transition-duration: .3s;
+
+}
+
 
 </style>
--- src/views/TeamsPage.vue
+++ src/views/TeamsPage.vue
@@ -17,33 +17,48 @@ export default {
             teams : [
                 { 
                     id : 1,
-                    name : 'Juventus',
-                    coach : 'someGuy'
-                    //img : this.infoTeams[0].teams[0].strTeamBadge
+                    name : 'Arsenal',
+                    coach : 'someGuy',
+                    img : '',
+                    stadium : '',
+                    formed : '',
+                    description: ''
                 },
                 { 
                     id : 2,
-                    name : 'Arsenal',
-                    coach : 'someGuy'
-                    //img : this.infoTeams[1].teams[0].strTeamBadge
+                    name : 'Liverpool',
+                    coach : 'someGuy',
+                    img : '',
+                    stadium : '',
+                    formed : '',
+                    description: ''
                 },
                 {
                     id : 3,
                     name : 'Inter',
-                    coach : 'someGuy'
-                   // img : this.infoTeams[2].teams[0].strTeamBadge
+                    coach : 'someGuy',
+                    img : '',
+                    stadium : '',
+                    formed : '',
+                    description: ''
                 },
                 {
                     id : 4,
                     name : 'Manchester United',
-                    coach : 'someGuy'
-                   // img : this.infoTeams[3].teams[0].strTeamBadge
+                    coach : 'someGuy',
+                    img : '',
+                    stadium : '',
+                    formed : '',
+                    description: ''
                 },
                 {
                     id : 5,
-                    name : 'AC Milan',
-                    coach : 'someGuy'
-                   // img : this.infoTeams[4].teams[0].strTeamBadge
+                    name : 'Barcelona',
+                    coach : 'someGuy',
+                    img : '',
+                    stadium : '',
+                    formed : '',
+                    description: ''
                 }
             ],
             infoTeams : []
@@ -63,9 +78,14 @@ export default {
                 .get('https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=' + this.teams[i].name)
                 .then(resp => resp.data);
                 this.infoTeams[i] = resp;
+                this.teams[i].img = this.infoTeams[i].teams[0].strTeamBadge;
+                this.teams[i].stadium = this.infoTeams[i].teams[0].strStadium;
+                this.teams[i].formed = this.infoTeams[i].teams[0].intFormedYear;
+                this.teams[i].description = this.infoTeams[i].teams[0].strDescriptionEN;
+                console.log(this.teams[i]);
                 
             }
-            console.log(this.infoTeams[1].teams[0]);
+            
 
         } catch (error) {
             console.error(error);
