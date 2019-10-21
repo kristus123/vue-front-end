645adfb5447a922f6cd6937700de68b897789bb4
Truls <trugunn_css@hotmail.com>
2019-10-21 11:20:43 +0200

    trying to get all the persons that dont have a player.

--- src/components/admin/player/AddPlayer.vue
+++ src/components/admin/player/AddPlayer.vue
@@ -15,62 +15,93 @@ export default {
         FlexibleForm
     },
 
-    methods: {
-        submitForm (value) {
-        console.log("____________")
-        console.log(value) // someValue
-        }
-    },
+  beforeMount: async function() {
+    let people = await personService.getPerson();
+    
+    let players = await playerService.findAll();
 
-    data() {
-        return {
-            textColor: "text-black",
-            image: require(`@/assets/action-adult-athlete-1311619.jpg`),
-            inputs: [
-                {
-                    title: "Player ID",
-                    placeholder: "Enter a player ID",
-                    type: "number",
-                    required: true,
-                    disabled: true,
-                    icon: "fas fa-running"
-                },
-                {
-                    title: "Person ID",
-                    placeholder: "Enter a person ID",
-                    type: "number",
-                    required: true,
-                    disabled: true,
-                    icon: "fas fa-user"
-                },
-                {
-                    title: "Team ID",
-                    placeholder: "Enter a team ID",
-                    type: "number",
-                    required: true,
-                    disabled: false,
-                    icon: "fas fa-users"
-                },
-                {
-                    title: "Normal Position",
-                    placeholder: "Enter a position",
-                    type:"text",
-                    required: true,
-                    disabled: false,
-                    icon: "fas fa-layer-group"
-                },
-                {
-                    title: "Player Number",
-                    placeholder: "Enter a player number",
-                    type: "number",
-                    required: "required",
-                    disabled: false,
-                    icon: "fas fa-hashtag"
-                }
-            ]
+    let playersID = [];
+    let personID = [];
+
+    let nonPlayers = [];
+
+    for(var i = 0; i < people.length; i++) {
+      for(var j = 0; j < players.length; j++) {
+
+
+
+      }
+
+
+    }
+
+
+    /*for(var i = 0; i < players.length; i++) {
+      playersID.push(players[i].person.personId);
+    }
+
+    for(var i = 0; i < people.length; i++) {
+      personID.push(people[i].personId);
+    }
+
+    for(var i = 0; i < personID.length; i++) {
+      if(playersID.includes(personID[i])){
+        console.log("Person is allready assigned a player");
+      } else {
+          nonPlayers.push(personID[i]);
+      }
+    }
+    
+
+
+    for(var i = 0; i < people.length; i++) {
+      for(var j = 0; j < nonPlayers.length; j++) {
+        if(people[i].personId === nonPlayers[j]) {
+          this.options.push(people[i]);
         }
-    }    
-}
+      }
+    }
+
+    console.log(this.options);*/
+
+
+
+  },
+
+
+  methods: {
+    submitForm(value) {
+      playerService.create(value);
+    }
+  },
+
+  data() {
+    return {
+      textColor: "text-black",
+      image: require(`@/assets/action-adult-athlete-1311619.jpg`),
+      inputs: [
+        {
+          title: "Normal Position",
+          placeholder: "Enter a position",
+          type: "text",
+          required: true,
+          disabled: false,
+          icon: "fas fa-layer-group"
+        },
+        {
+          title: "Player Number",
+          placeholder: "Enter a player number",
+          type: "number",
+          required: "required",
+          disabled: false,
+          icon: "fas fa-hashtag"
+        }
+      ],
+      options: [],
+
+    };
+  }
+};
 </script>
 
 <style>
