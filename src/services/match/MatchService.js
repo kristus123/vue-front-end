0f81e62448ba2112bc930d6cb846adbf9eecc4f1
haakon <haakon_underdal@hotmail.com>
2019-10-25 09:30:18 +0200

    trying to fix

--- src/services/match/MatchService.js
+++ src/services/match/MatchService.js
@@ -1,22 +1,12 @@
-import instance from '@/services/AxiosService';
+import instance from '@/services/AxiosService'
 
 class MatchService {
 
-    findAll() {
-        return instance.get("/v1/common/get/match").then(response => response.data);
+    getMatch(id) {
+        return instance.get(`/v1/admin/get/match/${id}`)
+            .then(response => response.data);
     }
-
-    update(matchObject, matchId) {
-
-        return instance.put(`/v1/admin/update/match/${matchId}`, matchObject).then(response => response);
-
-    }
-
-    create(matchObject) {
-        return instance.post("/v1/admin/post/match", matchObject).then(response => response);
-    }
-
-
 }
+
 const matchService = new MatchService();
 export default matchService;
