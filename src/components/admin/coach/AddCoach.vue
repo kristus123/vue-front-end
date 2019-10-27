2abb00fec4cd34fe0ba0eb3dd0b1518af3d71850
Truls Gunnerød <trugunn_css@hotmail.com>
2019-10-27 22:27:46 +0100

    almost finished up the add coach function.

--- src/components/admin/coach/AddCoach.vue
+++ src/components/admin/coach/AddCoach.vue
@@ -1,55 +1,158 @@
 <template>
   <b-container>
-    <h1>Add a COACH</h1>
-    <flexible-form
-      :inputs="inputs"
-      width="100%"
-      :image="image"
-      :color="textColor"
-      @clicked="submitForm"
-    />
+    <h1>Add a coach</h1>
+    <b-row class="justify-content-center">
+      <b-col cols="12">
+        <flexible-form
+          :image="image"
+          :color="textColor"
+          :showBtns="onShowBtns"
+          @clicked="submitForm"
+        >
+          <template v-slot:firstDropdown>
+            <b-form-row class="justify-content-center">
+              <b-col cols="8">
+                <b-form-group class="text-white" label="Pick a person" style="text-align: left;">
+                  <b-input-group>
+                    <form-select :options="personOptions" :preslecet="preselectPerson" v-on:DropDownValue="onSelectPerson"/>
+                  </b-input-group>
+                </b-form-group>
+              </b-col>
+            </b-form-row>
+          </template>
+
+          <template v-slot:teamDropdown>
+            <b-form-row class="justify-content-center">
+              <b-col cols="8">
+                <b-form-group class="text-white" label="Pick a team" style="text-align: left;">
+                  <b-input-group>
+                    <form-select :options="teamOptions" :preselect="preselectTeam" v-on:DropDownValue="onSelectTeam"/>
+                  </b-input-group>
+                </b-form-group>
+              </b-col>
+            </b-form-row>
+          </template>
+
+
+
+        </flexible-form>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
 import FlexibleForm from "@/components/forms/FlexibleForm";
-import coachService from '@/services/coach/CoachService.js';
+import FormSelect from "@/components/forms/FormSelect";
+import coachService from '@/services/coach/CoachService';
+import personService from '@/services/person/PersonService';
 import formService from '@/services/form/FormService.js';
+import teamService from "@/services/team/TeamService";
 
 export default {
   name: "Addplayer",
   components: {
-    FlexibleForm
+    FlexibleForm,
+    FormSelect
+  },
+
+  async beforeMount() {
+    await this.getPeople();
+    await this.getTeams();
   },
 
   methods: {
     submitForm(value) {
       coachService.turnPersonIntoCoach(value);
+    },
+
+    onSelectPerson(value) {
+      this.preselectPerson = value;
+      this.onShowBtns = true;
+    },
+
+    onSelectTeam(value) {
+      this.preselectTeam = value;
+    },
+
+    async getPeople() {
+      let allPeople = await personService.getPerson();
+      let people = [];
+
+      for(var i = 0; i < allPeople._embedded.personModelList.length; i++) {
+          people[i] = {
+            personId: allPeople._embedded.personModelList[i].personId,
+            firstName: allPeople._embedded.personModelList[i].firstName,
+            lastName: allPeople._embedded.personModelList[i].lastName
+          }
+      }
+      this.filterPeople(people);
+    },
+
+    async filterPeople(people) {
+      let coaches = await coachService.findAll();
+      let filteredPeople = [];
+      let options = [];
+
+      for(var i = 0; i < people.length; i++) {
+        if(!coaches.some(item => item.person.personId === people[i].personId)) {
+          filteredPeople.push(people[i]);
+        }
+      }
+
+      for(var i = 0; i < filteredPeople.length; i++) {
+        if(i === 0) {
+          options[i] = {
+            value: null,
+            text: 'Please pick a person',
+            disabled: true
+          }
+        }
+
+        options[i+1] = {
+          value: filteredPeople[i].personId,
+          text: filteredPeople[i].firstName + " " + filteredPeople[i].lastName,
+          disabled: false
+        }
+      }
+
+      this.personOptions = options;
+
+    },
+
+    async getTeams() {
+      let teams = await teamService.findAll();
+      console.log(teams);
+      let options = [];
+
+      for(var i = 0; i < teams.length; i++) {
+        if(i === 0) {
+            options[i] = {
+            value: null,
+            text: 'Please pick a team',
+            disabled: true
+          }
+        }
+        
+        options[i+1] = {
+          value: teams[i].teamId,
+          text: teams[i].association.name,
+          disabled: false
+        }
+      }
+      this.teamOptions = options;
     }
   },
 
   data() {
     return {
-      textColor: "text-black",
-      image: require(`@/assets/action-adult-athlete-1311619.jpg`),
-      inputs: [
-        {
-          title: "Person Id",
-          placeholder: "Select who you want to be a coach",
-          type: "number",
-          required: true,
-          disabled: false,
-          icon: "fas fa-users"
-        },
-        {
-          title: "Which team should the person coach ? (ID) ",
-          placeholder: "Select a team",
-          type: "number",
-          required: true,
-          disabled: false,
-          icon: "fas fa-users"
-        },
-      ]
+      textColor: "text-white",
+      image: require(`@/assets/adult-agenda-black-1543924.jpg`),
+      personOptions: [],
+      teamOptions: [],
+      preselectTeam: null,
+      preselectPerson: null,
+      onShowBtns: false
     };
   }
 };
