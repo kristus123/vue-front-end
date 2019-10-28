7fb16dc2bc5e694f7115493731a91c11b3c1a638
krispetter@gmail.com <krispetter@gmail.com>
2019-10-28 08:49:40 +0100

    chaneges

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -8,7 +8,7 @@ const headers = {
 }
 
 const instance = axios.create({
-    baseURL: process.env.API_URL || 'http://localhost:8090', 
+    baseURL: process.env.API_URL, 
     headers: headers
 }
 ); // http://localhost:8090 // https://young-everglades-84944.herokuapp.com/
