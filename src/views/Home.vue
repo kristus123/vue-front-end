6437172be000615f240c019d1b9a3781dc9edab3
krispetter@gmail.com <krispetter@gmail.com>
2019-10-28 08:57:40 +0100

    changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,6 +31,7 @@
     <div class="home" style="max-width:100%; overflow:hidden;">
       <div>
         <h1 class="black">Welcome to ProFotball!</h1>
+        {{process.env.API_URL}}
         <hr class="pretty" />
       </div>
 
