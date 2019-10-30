f64ba9a13620aefc22744c8ed33163f9f08109ce
krispetter@gmail.com <krispetter@gmail.com>
2019-10-30 12:59:19 +0100

    changes

--- src/components/anonymous/BrowsePlayer.vue
+++ src/components/anonymous/BrowsePlayer.vue
@@ -1,96 +1,91 @@
 <template>
-  <div>
-    <h1 style="color:black;">One player</h1>
-    <hr class="pretty" />
-
-    <center>
-      <h3>Player Data</h3>
-      <b-row class="justify-content-center">
-        <b-col cols="8">
-          <form-select :options="playerOptions" v-on:DropDownValue="onSelectPlayer" />
-          <p><br>{{playerData}}</p>
-        </b-col>
-      </b-row>
+  <b-container>
+    <div style="max-width:100%;">
+      <h1 style="color:black;">One player</h1>
       <hr class="pretty" />
 
-      <b-container class="bv-example-row">
-        <b-row>
+      <center>
+        <h3>Player Data</h3>
+        <b-row class="justify-content-center">
           <b-col cols="8">
-            <div v-for="player in player">
-              <div style="max-width:100%; padding:20px;">
-                
-              </div>
-            </div>
+            <form-select :options="playerOptions" v-on:DropDownValue="onSelectPlayer" />
+            <!-- <p><br></p> -->
+            <h1 style="padding-top:40px;">{{playerData}}</h1>
           </b-col>
-
-          
         </b-row>
-      </b-container>
-    </center>
-  </div>
+        <hr class="pretty" />
+
+        <b-container class="bv-example-row">
+          <b-row>
+            <b-col cols="8">
+              <div v-for="player in player">
+                <div style="max-width:100%; padding:20px;"></div>
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
+      </center>
+    </div>
+  </b-container>
 </template>
 
 <script>
-import PlayerService from '@/services/player/PlayerService';
-import FormSelect from '@/components/forms/FormSelect';
+import PlayerService from "@/services/player/PlayerService";
+import FormSelect from "@/components/forms/FormSelect";
 
 export default {
-  
   async beforeMount() {
     this.getPlayers();
   },
-  
-  methods: {
 
+  methods: {
     async getPlayers() {
       let players = await PlayerService.findAll();
       let options = [];
-      
 
-      for(var i = 0; i < players.length; i++) {
+      for (var i = 0; i < players.length; i++) {
         console.log(players[i]);
         delete players[i]._links;
 
-        if(i === 0) {
+        if (i === 0) {
           options[i] = {
             value: null,
-            text: 'Please select a player',
+            text: "Please select a player",
             disabled: true
-          }
+          };
         }
 
-        options[i+1] = {
+        options[i + 1] = {
           value: players[i].playerId,
           text: players[i].playername,
-                    
+
           disabled: false
-        }
+        };
       }
 
       this.playerOptions = options;
-
     },
 
     async onSelectPlayer(value) {
       this.playerData = "";
-            this.player = await PlayerService.findAnonPlayerById(value); 
-            console.log(this.player);
-            this.playerData = this.player.playerName + " plays for " + this.player.teamName;
-            
-    },
+      this.player = await PlayerService.findAnonPlayerById(value);
+      console.log(this.player);
+      this.playerData =
+        this.player.playerName + " plays for " + this.player.teamName;
+    }
   },
 
   data() {
     return {
-        playerData: "", 
-        player: "",
-        searchString: "",
-        filteredList: this.players,
-        playerString: '',
-        playerOptions: []
+      playerData: "",
+      player: "",
+      searchString: "",
+      filteredList: this.players,
+      playerString: "",
+      playerOptions: []
     };
   },
-  components:{
+  components: {
     FormSelect
   }
 };
